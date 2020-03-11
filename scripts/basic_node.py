efd9032619f9dcde3373a51e7ce54ea82ef168f2
redvinaa <redvinaa@gmail.com>
2020-03-11 11:03:17 +0100

    get clickable pic bemasolva

--- scripts/basic_node.py
+++ scripts/basic_node.py
@@ -2,13 +2,15 @@
 # -*- coding: utf-8 -*-
 
 import rospy
-from geometry_msgs.msg         import PoseStamped, Pose
+from geometry_msgs.msg         import PoseStamped
 from porszilo_telepresence.srv import ClickedPoint
 from sensor_msgs.msg           import Image
 from nav_msgs.msg              import OccupancyGrid, Odometry
 import cv2
 from cv_bridge import CvBridge, CvBridgeError
 import numpy as np
+import math
+import matplotlib.pyplot as plt
 
 class Telepresence():
     ## global variables:
@@ -47,28 +49,154 @@ class Telepresence():
 
         self.getClickablePic()# fills self.clickable_pic
 
+        #  self.imagePub.publish(CvBridge().cv2_to_imgmsg(self.clickable_pic, encoding="passthrough"))
         self.imagePub.publish(CvBridge().cv2_to_imgmsg(self.clickable_pic, encoding="passthrough"))
-        pass
+        
     
     #!!!!!!!!!!!!!!!!!! már csak ez nincs meg
     def getClickablePic(self):
         # calculate 2D pic with the clickable areas marked
-        self.clickable_pic = self.cv_depth
-        #  rospy.loginfo("OccupancyGrid origin: \n{}".format(self.grid.info.origin))
-        pose = PoseStamped()
-        pose.header.frame_id = self.grid.header.frame_id
-        pose.header.stamp = rospy.Time.now()
-        #  rospy.loginfo("Grid frame id: {}".format(pose.header.frame_id))
-        pose.pose = self.grid.info.origin
-        self.orig_pub.publish(pose)
+	
+	
+	# az általam rajzolt térképen 1 pixel 1 cm a valóságban
 
-        #  self.grid_image = Image()
-        #  self.grid_image.header = self.grid.header
-        #  self.grid_image.height = self.grid.info.height
-        #  self.grid_image.width  = self.grid.info.width
-        #  self.grid_image.data   = self.grid.data
-        #  self.cv_grid_image = CvBridge().imgmsg_to_cv2(self.grid_image, desired_encoding="passthrough")
+	pos_y = int(round(self.odom.pose.pose.position.y/self.grid.info.resolution + self.grid.info.height/2)) # pozíciónk a térképen pixelben
+	pos_x = int(round(self.odom.pose.pose.position.x/self.grid.info.resolution + self.grid.info.width/2))
+	h = int(round(self.odom.pose.pose.position.z/self.grid.info.resolution))+5 # a kamer optikai tengelyének magassága a földtől pixel egységgé alakítva
+	view_angle_v = self.view_angle_v  # kamera vertikális látószöge fokban
+	view_angle_h = self.view_angle_h   # kamera horizontális látószöge fokban
 
+	qx=self.odom.pose.pose.orientation.x
+	qy=self.odom.pose.pose.orientation.y
+	qz=self.odom.pose.pose.orientation.z
+	qw=self.odom.pose.pose.orientation.w
+	siny_cosp = 2 * (qw * qz + qx * qy)
+	cosy_sinp = 1 - 2 * (qy * qy + qz * qz)
+	yaw = math.atan2(siny_cosp, cosy_sinp)
+	
+	deg = yaw * 180 / math.pi + 90 # hány fokkal kell elforgatnunk a térépet hogy a robot nézési iránya függőlegesen legyen
+	rospy.loginfo(deg)
+
+	orig_map = np.array(self.grid.data)  # beolvassuk a térképet és a látott képet
+	orig_map = np.where(orig_map==-1, 0, 255)
+	
+	orig_map = np.reshape(orig_map,(self.grid.info.height, self.grid.info.width)).astype('uint8')
+	orig_map = cv2.flip(orig_map, 0)
+
+	
+	cam = self.cv_color
+
+	self.clickable_pic = orig_map
+	#cv2.imshow("a", orig_map)
+	#cv2.waitKey()
+	
+	#plt.imshow(orig_map)
+	#plt.show()
+
+
+	cam_gray = cv2.cvtColor(cam, cv2.COLOR_BGR2GRAY)
+
+	# cv2.imshow('map', map)
+	# cv2.waitKey()
+
+	map_cols = orig_map.shape[1]
+	map_rows = orig_map.shape[0]
+
+	cam_cols = cam.shape[1]
+	cam_rows = cam.shape[0]
+
+	map_diag = int(round(1.2 * (math.sqrt(math.pow(map_cols, 2) + math.pow(map_rows, 2)))))  # egy nagy térkép amibe beletéve a kicsit szabadon el tudjuk forgatni
+	big_map = np.zeros((map_diag, map_diag), 'uint8')
+
+	shift_cols = int(round((map_diag - map_cols) / 2))  # beletesszük a közepére a térképet
+	shift_rows = int(round((map_diag - map_rows) / 2))
+	big_map[shift_rows:(map_rows + shift_rows), shift_cols:(map_cols + shift_cols)] = orig_map
+
+	pos_y_big = shift_rows + pos_y  # átszámítva a koordinátkat a nagy térképre
+	pos_x_big = shift_cols + pos_x
+
+	# big_map[pos_y_big,pos_x_big]=0
+	# cv2.imshow('big_map', big_map)
+	# cv2.waitKey()
+
+
+	M_rot = cv2.getRotationMatrix2D((map_diag / 2, map_diag / 2), deg, 1)  # elforgatjuk hogy függőlegesen fölfelé legyen a robot nézési iránya
+	map_rot = cv2.warpAffine(big_map, M_rot, (map_diag, map_diag))
+
+	# Hol vagyunk a forgatás után, a nagy térképen?
+	big_map_half = int(round(map_diag / 2))
+	rad = deg * math.pi / 180
+	pos_x_rot = int((pos_x_big - big_map_half) * math.cos(rad) + (pos_y_big - big_map_half) * math.sin(rad) + big_map_half)
+	pos_y_rot = int(-(pos_x_big - big_map_half) * math.sin(rad) + (pos_y_big - big_map_half) * math.cos(rad) + big_map_half)
+
+	# map_rot[pos_y_rot, pos_x_rot] = 0
+	# cv2.imshow('map_rot', map_rot)
+	# cv2.waitKey()
+
+
+	# keressük meg a perspektívikusan torzítandó képrészlet 4 sarkát
+	# ez itt puszta koordinátageometria kiegyszerűsítve, átláthatatlanul
+
+	# print(pos_y_rot,pos_x_rot)
+
+	y3 = pos_y_rot - int(round(h * math.tan((90 - view_angle_v / 2) * math.pi / 180)))  # 324
+	y4 = np.where(big_map == 255)[0][0]  # 14
+
+	rospy.loginfo("view_angle_h = {}".format(view_angle_h))
+	assert(view_angle_h != 180)
+	m = math.tan((90 - view_angle_h / 2) * math.pi / 180)  # 1.28
+	b1 = pos_y_rot + m * pos_x_rot  # 711.6
+	b2 = pos_y_rot - m * pos_x_rot  # 20.4
+
+	assert(m)
+	p1x = int(round((y3 - b2) / m))
+	p1y = int(round(m * p1x + b2))
+
+	p2x = int(round((y4 - b2) / m))
+	p2y = int(round(m * p2x + b2))
+
+	p3x = int(round((-y4 + b1) / m))
+	p3y = int(round(-m * p3x + b1))
+
+	p4x = int(round((-y3 + b1) / m))
+	p4y = int(round(-m * p4x + b1))
+
+	# map_rot[p1y, p1x] = 0
+	# map_rot[p4y, p4x] = 0
+	# map_rot[p2y, p2x] = 255
+	# map_rot[p3y, p3x] = 255
+	# cv2.imshow('map_rot2', map_rot)
+	# cv2.waitKey()
+
+	# mennyire kell összenyomni a képet
+	beta = math.atan((pos_y_rot - y4) / h) * 180 / math.pi - 90 + view_angle_v / 2
+	ratio = beta / view_angle_v
+	print(ratio)
+
+	output_rows = int(round(cam_rows * ratio))
+	output_cols = int(round(cam_cols))
+
+	pts1 = np.float32([[p2x, p2y], [p3x, p3y], [p1x, p1y], [p4x, p4y]])
+	pts2 = np.float32([[0, 0], [output_cols, 0], [0, output_rows], [output_cols, output_rows]])
+	M_persp = cv2.getPerspectiveTransform(pts1, pts2)
+	output = cv2.warpPerspective(map_rot, M_persp, (output_cols, output_rows))
+	output = output
+
+	output2 = np.array([cam_gray], dtype=int)
+	output2[0, cam_rows - output_rows - 1:-1, :] = output
+	output2 = np.where(output2 == 0, cam_gray, output2)
+
+	final2 = np.zeros((cam_rows, cam_cols, 3))
+	final2[:, :, 0] = cam_gray
+	final2[:, :, 1] = output2
+	final2[:, :, 2] = cam_gray
+
+
+	#plt.matshow(final2)
+	#  cv2.imshow("lofasz", final2 / 255.0)
+	#  cv2.waitKey(1)
+	#cv2.imwrite("D:\\BME\\MSZO\\hostess robot\\2020\\kep2_res_rgb.jpg", final2)
+	self.clickable_pic = final2
 
 
     #!!!!!!!!!!!!!!!!!! meg ez
@@ -90,34 +218,18 @@ class Telepresence():
         rospy.loginfo("Clicked point: {}, {}".format(self.point_x, self.point_y))
         #  rospy.loginfo("Color width: {}, height: {}".format(self.color.width, self.color.height))
         #  rospy.loginfo("Depth width: {}, height: {}".format(self.depth.width, self.depth.height))
-        #  rospy.loginfo("Shape: {}".format(self.cv_depth.shape))
-        #  rospy.loginfo("Clicked depth: {}".format(self.cv_depth[self.point_y, self.point_x]))
-
-        #  neigh = 5
-        #  rospy.loginfo("Neighborhood of clicked point: \n{}".format(self.cv_depth[self.point_y-neigh:self.point_y+neigh, self.point_x-neigh:self.point_x+neigh]))
+        rospy.loginfo("Shape: {}".format(self.cv_depth.shape))
+        rospy.loginfo("Clicked depth: {}".format(self.cv_depth[self.point_y, self.point_x]))
 
         #  cv2.imshow("anyad", self.cv_depth == np.nan)
-        #  tmp = np.array(self.cv_depth)
-        #  cv2.waitKey(0)
-        #  cv2.destroyAllWindows()
+        tmp = np.array(self.cv_depth)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
         #########################################################################
         ####################      ezzel szarakodtunk ma      ####################
         #########################################################################
 
-        img_width  = 700
-        img_height = 500 # az index.html-ből
-
-        actual_clicked_width  = int((self.point_y - 1) * self.depth.width / img_width)
-        actual_clicked_height = int(self.point_x * self.depth.height / img_height)
-
-        rospy.loginfo("actual_clicked: {}, {}".format(actual_clicked_width, actual_clicked_height))
-        ## még ez sem működik
-
-        idx = actual_clicked_width * self.depth.width + actual_clicked_height
-        clicked_data = self.depth.data[idx]
-        rospy.loginfo("Got depth: {}".format(clicked_data))
-        rospy.loginfo("Type of got depth: {}".format(type(clicked_data)))
 
         self.goal.header.stamp = rospy.Time.now()
         #  self.goal.pose.position.x = ...
@@ -139,9 +251,9 @@ class Telepresence():
     def main(self):
         rospy.init_node('telepresence')
     
-        self.freq = rospy.get_param("~freq", 4) # looped at 4 Hz by default
-        self.view_angle_v = rospy.get_param("~view_angle_v")
-        self.view_angle_h = rospy.get_param("~view_angle_h")
+        freq = rospy.get_param("frequency", 10) # default 10 Hz
+	self.view_angle_v = rospy.get_param("view_angle_v", 50)
+	self.view_angle_h = rospy.get_param("view_angle_h", 76)
     
         rospy.loginfo('Waiting for messages to arrive...')
         try:
@@ -162,7 +274,6 @@ class Telepresence():
         rospy.Subscriber("/move_base/global_costmap/costmap", OccupancyGrid, self.cbGrid)
         rospy.Subscriber("/odometry/filtered", Odometry, self.cbOdom)
     
-        self.orig_pub = rospy.Publisher("origin", PoseStamped, queue_size=1)
         rospy.loginfo('All messages have arrived at least once, starting publishers')
         self.goalPub  = rospy.Publisher("/move_base_simple/goal", PoseStamped, queue_size=5)
         self.imagePub = rospy.Publisher("/telepresence/image", Image, queue_size=5)
@@ -170,7 +281,7 @@ class Telepresence():
         # think of this as a remote function call, called by the  JS code in the browser
         self.srv_clicked_point = rospy.Service("/telepresence/clicked_point", ClickedPoint, self.cbClickedPoint)
     
-        rate = rospy.Rate(self.freq)
+        rate = rospy.Rate(freq)
         try:
             while not rospy.is_shutdown():
                 self.run()# <- this is being looped
